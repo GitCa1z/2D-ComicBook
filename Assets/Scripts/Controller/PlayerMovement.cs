5636a1156b8dba58775d05ce2940b89416da5e3f
GitCa1z <cjbowie18@gmail.com>
2024-12-07 12:36:26 +0000

    WallJumping
    Added wall jump and wall slide to player movement

--- Assets/Scripts/Controller/PlayerMovement.cs
+++ Assets/Scripts/Controller/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -10,17 +11,32 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField] public  Transform groundCheck;
   [SerializeField] public  LayerMask groundMask;
   [SerializeField] public  TrailRenderer trilRenderer;
+  [SerializeField] public Transform wallCheck;
+  [SerializeField] public LayerMask wallMask;
 
     private float horizontal;
     private float speed = 8f;
-    private float jumpPower = 16f;
+    private float jumpPower = 8f;
     private bool isFacingRight = true;
+    [SerializeField ]private int extraJumps = 1;
+
+
 
     private bool canDash = true;
     private bool isDashing;
+    private bool isWallSliding;
+    private bool isWallJumping;
+
+    private float wallSlidingSpeed = 2f;
     private float dashPower = 24f;
     private float dashingTime = 0.2f;
     private float dashingCoolDown = 1f;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(16f, 8f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,38 +46,112 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Flip();
+        JumpReset();
+        wallJump();
+
+        if (!isWallJumping)
+        {
+            Flip();
+
+        }
+
+
+    }
+
+    private void JumpReset()
+    {
+        if (isGrounded()) extraJumps = 1;
     }
 
     private void FixedUpdate()
     {
-        if (isDashing)
+        if (isDashing )
         {
             return;
         }
 
-        rb2D.velocity = new Vector2(horizontal * speed, rb2D.velocity.y);
+        if (!isWallJumping)
+        {
+         rb2D.velocity = new Vector2(horizontal * speed, rb2D.velocity.y);
+        }
 
         
+
+        wallSliding();
+
+       
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded())
+
+        if (context.performed && extraJumps > 0)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpPower);
+            extraJumps--;
+           
         }
 
-        if (context.canceled && rb2D.velocity.y > 0f) 
-        { 
-            rb2D.velocity = new Vector2(rb2D.velocity.x,rb2D.velocity.y * 0.5f);
+        if (wallJumpingCounter > 0)
+        {
+            isWallJumping = true;
+            rb2D.velocity = new Vector2 (wallJumpingDirection * wallJumpingPower.x,wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDuration)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+
+            }
+
+            Invoke(nameof(stopWallJumping), wallJumpingDirection);
+
         }
 
     }
 
-    private bool isGrounded()
+    public bool isGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundMask);
+    }
+
+    public bool isWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallMask);
+    }
+
+    public void wallSliding()
+    {
+        if (isWalled() && !isGrounded() && horizontal != 0) { 
+            isWallSliding = true;
+            rb2D.velocity = new Vector2(rb2D.velocity.x, Mathf.Clamp(rb2D.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        else isWallSliding = false;
+    }
+
+    private void wallJump()
+    {
+        if (isWallSliding) { 
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(stopWallJumping));
+        }
+
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+    }
+
+    private void stopWallJumping()
+    {
+        isWallJumping = false;
     }
 
     private void Flip()
@@ -85,6 +175,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void Dashing(InputAction.CallbackContext context)
     {
+        
         StartCoroutine(Dash());
     }
 
